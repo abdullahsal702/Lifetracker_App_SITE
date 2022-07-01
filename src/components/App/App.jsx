c8847acb680e9ed1a7bc1fb286620ede53fa3a3c
abdullahsal702 <88620119+abdullahsal702@users.noreply.github.com>
2022-07-01 13:03:20 -0400

    login/register works

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,19 +4,45 @@ import Navbar from "../Navbar/Navbar"
 import Landing from "../Landing/Landing"
 import Login from "../Login/Login"
 import Register from "../Register/Register"
+import { useState, useEffect } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
 function App() {
+
+  // const [firstName, setFirstName] = useState("")
+  // const [lastName, setLastName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [usernameame, setUsername] = useState("")
+  // const [password, setPassword] = useState("")
+
+  // const handleOnTextChange = (event) => {
+  //   if (event.target.name == "email") {
+  //     setEmail(event.target.value)
+  //   } else if (event.target.name == "fname") {
+  //     setFirstName(event.target.value)
+  //   } else if (event.target.name == "lname") {
+  //     setLastName(event.target.value)
+  //   } else if (event.target.name == "username") {
+  //     setUsername(event.target.value)
+  //   } else if (event.target.name == "password") {
+  //     setPassword(event.target.value)
+  //   } 
+  // }
+
+  const [user, setUser] = useState({}) 
+
+
   return (
     <div className="app">
       <BrowserRouter>
         <main>
           <Navbar/> 
           <Routes>
+            {/* add conditional inside element prop to render either activity or redirect to login */}
             <Route path="/" element={<Landing/>}></Route>
-            <Route path="/login" element={<Login/>}></Route>
-            <Route path="/register" element={<Register/>}></Route>
+            <Route path="/login" element={<Login user={user} setUser={setUser}/>}></Route>
+            <Route path="/register" element={<Register user={user} setUser={setUser}/>}></Route>
           </Routes> 
         </main>
       </BrowserRouter>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,39 +1,81 @@
 import "./Register.css"
 import * as React from "react"
-import { Link } from "react-router-dom"
+import apiClient from "../../services/apiClient"
+import { useState, useEffect } from "react" 
+import { useNavigate, Link } from "react-router-dom"
  
-export default function Register() {
+export default function Register({ user, setUser }) {
+    const navigate = useNavigate()
+    const [isProcessing, setIsProcessing] = useState(false)
+    const [errors, setErrors] = useState({})
+    const [form, setForm] = useState({
+        email: "",
+        username: "", 
+        firstName: "", 
+        lastName: "",
+        password: "",
+        passwordConfirm: ""
+    })
+
+    useEffect(() => {
+        //should navigate to /activity 
+        if (user?.email) {
+            navigate("/")
+        }
+    }, [user, navigate])
+
+    const handleOnTextChange = (event) => {
+        // add errors, passwords dont match, invalid email, etc., part 11
+        setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
+    } 
+
+    const handleOnSubmit = async () => {
+        setIsProcessing(true)
+        setErrors((e) => ({...e, form: null}))
+        //add error checking 
+        const { data, error } = await apiClient.registerUser({ email: form.email, username: form.username, firstName: form.firstName, lastName: form.lastName, password: form.password })
+        if (error) {
+            setErrors((e) => ({ ...e, form: error }))
+        }
+        if (data?.user) {
+            setUser(data.user)
+            apiClient.setToken(data.token)
+        }
+        setIsProcessing(false)
+    }
+
+
     return (
         <div className="register">
             <div className="create">
-                <div class="material-icons">account_circle</div>
+                <div className="material-icons">account_circle</div>
                 <h1>Create An Account</h1>
                 <div className="register-form">
                     <div className="input-field">
                         <i className="fa fa-envelope"></i>
-                        <input type="email" name="email" placeholder="Email" value=""/>
+                        <input type="email" name="email" placeholder="Email" onChange={handleOnTextChange}/>
                     </div>
                     <div className="input-field">
                         <i className="fa fa-user"></i>
-                        <input type="text" name="username" placeholder="Username" value=""/>
+                        <input type="text" name="username" placeholder="Username" onChange={handleOnTextChange}/>
                     </div>
                     <div className="name"> 
                         <div className="input-field fname">
-                            <input type="text" name="fname" placeholder="First Name" value=""/>
+                            <input type="text" name="firstName" placeholder="First Name" onChange={handleOnTextChange}/>
                         </div>
                         <div className="input-field lname">
-                            <input type="text" name="lname" placeholder="Last Name" value=""/>
+                            <input type="text" name="lastName" placeholder="Last Name" onChange={handleOnTextChange}/>
                         </div>
                     </div>
                     <div className="input-field">
                         <i className="fa fa-lock"></i>
-                        <input type="password" name="password" placeholder="Password" value=""/>
+                        <input type="password" name="password" placeholder="Password" onChange={handleOnTextChange}/>
                     </div>
                     <div className="input-field">
-                        <input type="password" name="confirm-password" placeholder="Confirm Password" value=""/>
+                        <input type="password" name="passwordConfirm" placeholder="Confirm Password" onChange={handleOnTextChange}/>
                     </div>
                     <div className="input-field">
-                        <button className="register-btn">Sign up</button>
+                        <button className="register-btn" onClick={handleOnSubmit}>Sign up</button>
                     </div>
                 </div>
                 <div className="login-redirect">
